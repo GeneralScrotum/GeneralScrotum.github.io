03cd00b035206936c7db1016945b6b4d1c1ffb7c
GeneralScrotum <general.scrotum@gmx.at>
2017-02-15 09:31:14 +0100

    First Edition

---
+++ goenner.js
@@ -0,0 +1,6 @@
+
+    var i=0;
+    for(i=0; i<10; i++){
+        document.write("<div class=\"p_grey\">Der Gönner hat sich seine Grades fett ausgebessert.</div>"
+                      +"<div class=\"p_white\">Der Gönner hat sich seine Grades fett ausgebessert.</div>")
+     }
---
+++ seite_2/login.js
@@ -0,0 +1,8 @@
+function check(form){
+    if(form.userid.value == "sees" && form.pswrd.value == "saas"){
+        window.open("real_page/real_page.html");
+    }
+    else{
+        alert("Falsch du Fotze");
+    }
+}
